359825ad37e1b660a078f882f4b8c328865dc631
Juan Pablo <jguevara@cesde.net>
2024-04-17 16:27:28 -0500

    se realiza basico

---
+++ src/database/usuarios.js
@@ -0,0 +1,11 @@
+export let usuarios = [
+    {
+      nombre: "daniel puerta",
+      documento: "1017233498",
+      telefono: "correo@correo.com",
+      direccion: "carrera 74a #96-17",
+      correo: "correo@correo.com",
+      nota: 4.5,
+    },
+    
+  ];
